511e53e0ace8502224d3f47cce9bdd8f8db27963
hmercurin <mercurinh@gmail.com>
2024-10-30 15:18:43 +0100

    Configuration pour mise en ligne du site

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio-hugo/',
   server: {
     port: 3000,  // Spécifie le port 3000
   },
